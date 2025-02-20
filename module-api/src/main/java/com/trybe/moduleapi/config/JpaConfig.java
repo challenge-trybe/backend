f4ab9c14e6ca1ef4c06713e208a9aef70431afd9
rnignon <rnignon@naver.com>
2025-02-20 22:27:05 +0900

    chore: JpaConfig 파일 추가

---
+++ module-api/src/main/java/com/trybe/moduleapi/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.trybe.moduleapi.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.trybe.modulecore")
+@EntityScan(basePackages = "com.trybe.modulecore")
+public class JpaConfig {
+}
