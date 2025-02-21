b988232b2c90a4b63d3ef1deb6ab3f62a1aeac02
rnignon <rnignon@naver.com>
2025-02-21 18:56:29 +0900

    feat: Challenge 내 상태, 카테고리를 표현하는 enum 추가

---
+++ module-core/src/main/java/com/trybe/modulecore/challenge/enums/ChallengeCategory.java
@@ -0,0 +1,16 @@
+package com.trybe.modulecore.challenge.enums;
+
+public enum ChallengeCategory {
+    SPORTS("문동"),
+    HOBBY("취미"),
+    DIET("식단"),
+    SAVING("저축"),
+    STUDY("공부"),
+    LIFE("생활");
+
+    private final String description;
+
+    ChallengeCategory(String description) {
+        this.description = description;
+    }
+}
