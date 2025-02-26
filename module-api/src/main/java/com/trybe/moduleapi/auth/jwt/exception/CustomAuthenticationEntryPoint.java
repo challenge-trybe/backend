d354895d2574a1779ded08496a23f36422b4329f
minjiwon <mjo4260@naver.com>
2025-02-27 01:23:01 +0900

    feat: module-api/JWT 인증 관련 DeniedHandler, EntryPoint 코드 수정

--- module-api/src/main/java/com/trybe/moduleapi/auth/jwt/exception/CustomAuthenticationEntryPoint.java
+++ module-api/src/main/java/com/trybe/moduleapi/auth/jwt/exception/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.trybe.moduleapi.auth.jwt.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trybe.moduleapi.common.api.ApiErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,10 +14,21 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("UNAUTHORIZED = " + authException.getMessage());
+        response.setContentType("application/json;charset=UTF-8");
+
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(HttpStatus.UNAUTHORIZED.value(), "로그인이 필요한 서비스입니다.");
+        String errorMessage = objectMapper.writeValueAsString(apiErrorResponse);
+        response.getWriter().write(errorMessage);
     }
 }
