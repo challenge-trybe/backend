82c40886acc83af8ebbc52b57c74ddfb82962d84
rnignon <rnignon@naver.com>
2025-02-26 15:44:59 +0900

    feat: ChallengeParticipation 관련 Exception 작성 - NotFoundChallengeParticipationException: 조건에 따른 Participation 정보를 찾을 수 없을 경우 해당 예외 발생 - InvalidParticipationStatusException: 허용되지 않는 Participation Status를 사용했을 경우 해당 예외 발생 - InvalidChallengeRoleActionException: 특정 행위가 가능한 Challenge Role이 아닐 경우 해당 예외 발생 - ChallengeFullException: 챌린지 수용 인원이 가득 찼을 경우 해당 예외 발생 - ChallengeParticipationFullException: 챌린지 참여 관련 인원이 가득 찼을 경우 해당 예외 발생

---
+++ module-api/src/main/java/com/trybe/moduleapi/challenge/exception/ChallengeFullException.java
@@ -0,0 +1,10 @@
+package com.trybe.moduleapi.challenge.exception;
+
+import com.trybe.moduleapi.common.api.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class ChallengeFullException extends BusinessException {
+    public ChallengeFullException() {
+        super("챌린지 인원이 가득 찼습니다.", HttpStatus.CONFLICT.value());
+    }
+}
---
+++ module-api/src/main/java/com/trybe/moduleapi/challenge/exception/NotFoundChallengeParticipationException.java
@@ -0,0 +1,10 @@
+package com.trybe.moduleapi.challenge.exception;
+
+import com.trybe.moduleapi.common.api.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class NotFoundChallengeParticipationException extends BusinessException {
+    public NotFoundChallengeParticipationException() {
+        super("존재하지 않는 챌린지 참여입니다.", HttpStatus.NOT_FOUND.value());
+    }
+}
