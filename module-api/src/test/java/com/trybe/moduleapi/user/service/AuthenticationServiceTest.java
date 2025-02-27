c03f666280b0e93e264b853cea6e2c013a0c4c66
minjiwon <mjo4260@naver.com>
2025-02-27 21:36:35 +0900

    test: module-api/AuthenticationServiceTest 메서드명 수정

--- module-api/src/test/java/com/trybe/moduleapi/user/service/AuthenticationServiceTest.java
+++ module-api/src/test/java/com/trybe/moduleapi/user/service/AuthenticationServiceTest.java
@@ -41,8 +41,8 @@ class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
 
     @Test
-    @DisplayName("로그인을 성공하면 200을 반환한다.")
-    void 로그인_성공하면_200을_반환한다 () {
+    @DisplayName("로그인을 성공하면 유저 정보와 토큰을 반환한다.")
+    void 로그인_성공하면_유저정보와_토큰을_반환한다 () {
         /* given */
         LoginRequest request = AuthenticationFixtures.로그인_요청;
         UserFixtures.회원.updatePassword(UserFixtures.회원_암호화된_비밀번호);
@@ -59,8 +59,8 @@ class AuthenticationServiceTest {
     }
 
     @Test
-    @DisplayName("로그인을 실패하면 401을 반환한다.")
-    void 로그인_실패하면_401을_반환한다 () {
+    @DisplayName("로그인을 실패하면 에러를 반환한다.")
+    void 로그인_실패하면_에러를_반환한다 () {
         /* given */
         LoginRequest request = AuthenticationFixtures.로그인_요청;
         UserFixtures.회원.updatePassword(UserFixtures.새로운_암호화된_비밀번호);
@@ -73,8 +73,8 @@ class AuthenticationServiceTest {
     }
 
     @Test
-    @DisplayName("로그아웃을 성공하면 200을 반환한다")
-    void 로그아웃을_성공하면_200을_반환한다() {
+    @DisplayName("로그아웃을 성공하면 DB에서 리프레쉬토큰을 삭제한다.")
+    void 로그아웃을_성공하면_DB에서_리프레쉬토큰을_삭제한다() {
         /* given */
         Mockito.when(customUserDetails.getUser()).thenReturn(UserFixtures.회원);
 
@@ -86,8 +86,8 @@ class AuthenticationServiceTest {
     }
     
     @Test
-    @DisplayName("토큰 재발급을 성공하면 200을 반환한다.")
-    void 토큰_재발급을_성공하면_200을_반환한다() {
+    @DisplayName("토큰 재발급을 성공하면 유저정보와 새로운 토큰을 반환한다.")
+    void 토큰_재발급을_성공하면_유저정보와_새로운_토큰을_반환한다() {
         /* given */
         Mockito.when(jwtUtils.getUserId(AuthenticationFixtures.refreshToken)).thenReturn(UserFixtures.회원_아이디);
         Mockito.when(userRepository.findByUserId(UserFixtures.회원_아이디)).thenReturn(Optional.of(UserFixtures.회원));
