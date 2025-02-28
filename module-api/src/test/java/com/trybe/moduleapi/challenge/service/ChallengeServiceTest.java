b247bd05b15e9e0da8b727debc6890d6b62e4bea
rnignon <rnignon@naver.com>
2025-02-28 16:14:56 +0900

    test: 챌린지 삭제 테스트에 verify 추가하여 검증

--- module-api/src/test/java/com/trybe/moduleapi/challenge/service/ChallengeServiceTest.java
+++ module-api/src/test/java/com/trybe/moduleapi/challenge/service/ChallengeServiceTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ChallengeServiceTest {
@@ -176,6 +176,8 @@ class ChallengeServiceTest {
         /* when */
         /* then */
         challengeService.delete(challengeId, TMP_USER_ID);
+
+        verify(challengeRepository, atLeastOnce()).deleteById(challengeId);
     }
 
     @Test
