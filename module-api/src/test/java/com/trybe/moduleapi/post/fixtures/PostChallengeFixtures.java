81de7ed42a5b6bbf1f6f0b31ec99dfc0cfe5784b
minjiwon <mjo4260@naver.com>
2025-03-06 21:57:05 +0900

    test: 챌린지 오타 수정

--- module-api/src/test/java/com/trybe/moduleapi/post/fixtures/PostChallengeFixtures.java
+++ module-api/src/test/java/com/trybe/moduleapi/post/fixtures/PostChallengeFixtures.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class PostChallengeFixtures {
 
-    public static PostChallenge 포스트_첼린지 = new PostChallenge(PostFixtures.게시글, ChallengeFixtures.챌린지());
-    public static List<PostChallenge> 포스트_챌린지_목록 = List.of(포스트_첼린지);
+    public static PostChallenge 포스트_챌린지 = new PostChallenge(PostFixtures.게시글, ChallengeFixtures.챌린지());
+    public static List<PostChallenge> 포스트_챌린지_목록 = List.of(포스트_챌린지);
 
 }
