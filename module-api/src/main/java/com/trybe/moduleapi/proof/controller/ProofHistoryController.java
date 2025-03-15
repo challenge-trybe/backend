c5d5bf0048aa63766811b285287c6ac4b81900f3
rnignon <rnignon@naver.com>
2025-03-15 18:53:06 +0900

    feat: ProofHistory Controller 구현

---
+++ module-api/src/main/java/com/trybe/moduleapi/proof/controller/ProofHistoryController.java
@@ -0,0 +1,56 @@
+package com.trybe.moduleapi.proof.controller;
+
+import com.trybe.moduleapi.auth.CustomUserDetails;
+import com.trybe.moduleapi.common.dto.PageResponse;
+import com.trybe.moduleapi.proof.dto.request.ProofHistoryRequest;
+import com.trybe.moduleapi.proof.dto.response.ProofHistoryResponse;
+import com.trybe.moduleapi.proof.service.ProofHistoryService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/proofs/histories")
+public class ProofHistoryController {
+    private final ProofHistoryService proofHistoryService;
+
+    public ProofHistoryController(ProofHistoryService proofHistoryService) {
+        this.proofHistoryService = proofHistoryService;
+    }
+
+    @PostMapping("/{proofId}")
+    public ProofHistoryResponse.Summary save(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofId") Long proofId,
+            @Valid @RequestBody ProofHistoryRequest.Create request
+    ) {
+        return proofHistoryService.save(userDetails.getUser(),proofId, request);
+    }
+
+    @GetMapping("/{proofId}")
+    public PageResponse<ProofHistoryResponse.Summary> findAll(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofId") Long proofId,
+            Pageable pageable
+    ) {
+        return proofHistoryService.findAll(userDetails.getUser(), proofId, pageable);
+    }
+
+    @PutMapping("/{proofHistoryId}")
+    public ProofHistoryResponse.Summary update(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofHistoryId") Long proofHistoryId,
+            @Valid @RequestBody ProofHistoryRequest.Update request
+    ) {
+        return proofHistoryService.update(userDetails.getUser(), proofHistoryId, request);
+    }
+
+    @DeleteMapping("/{proofHistoryId}")
+    public void delete(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofHistoryId") Long proofHistoryId
+    ) {
+        proofHistoryService.delete(userDetails.getUser(), proofHistoryId);
+    }
+}
