04d4f4dad86ff295328c53b801b1050d7a3bfef5
minjiwon <mjo4260@naver.com>
2025-03-15 18:16:16 +0900

    feat: PostIds In절 조회 기능 추가

--- module-core/src/main/java/com/trybe/modulecore/post/repository/PostRepository.java
+++ module-core/src/main/java/com/trybe/modulecore/post/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.trybe.modulecore.post.repository;
 
 import com.trybe.modulecore.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+    List<Post> findAllByIdIn(Set<Long> postIds);
 }
