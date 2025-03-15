a2edd34cb8f32cbb3a25293c3a5d2924b52e3e07
rnignon <rnignon@naver.com>
2025-03-15 19:25:40 +0900

    feat: ProofHistoryVote Controller 구현

---
+++ module-api/src/main/java/com/trybe/moduleapi/proof/controller/ProofHistoryVoteController.java
@@ -0,0 +1,42 @@
+package com.trybe.moduleapi.proof.controller;
+
+import com.trybe.moduleapi.auth.CustomUserDetails;
+import com.trybe.moduleapi.proof.dto.response.ProofHistoryVoteResponse;
+import com.trybe.moduleapi.proof.service.ProofHistoryVoteService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/proofs/histories/votes")
+public class ProofHistoryVoteController {
+    private final ProofHistoryVoteService proofHistoryVoteService;
+
+    public ProofHistoryVoteController(ProofHistoryVoteService proofHistoryVoteService) {
+        this.proofHistoryVoteService = proofHistoryVoteService;
+    }
+
+    @PostMapping("/{proofHistoryId}")
+    public ProofHistoryVoteResponse.My save(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofHistoryId") Long proofHistoryId,
+            @RequestParam("approved") Boolean approved
+    ) {
+        return proofHistoryVoteService.save(userDetails.getUser(), proofHistoryId, approved);
+    }
+
+    @GetMapping("/{proofHistoryId}")
+    public ProofHistoryVoteResponse.My findMyVote(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofHistoryId") Long proofHistoryId
+    ) {
+        return proofHistoryVoteService.findMyVote(userDetails.getUser(), proofHistoryId);
+    }
+
+    @GetMapping("/{proofHistoryId}/result")
+    public ProofHistoryVoteResponse.Result findResult(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("proofHistoryId") Long proofHistoryId
+    ) {
+        return proofHistoryVoteService.getResult(userDetails.getUser(), proofHistoryId);
+    }
+}
