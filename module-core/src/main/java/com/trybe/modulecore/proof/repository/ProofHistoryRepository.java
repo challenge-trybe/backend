d787500f2b756f58cd3dd000edc1601e5d858a2f
rnignon <rnignon@naver.com>
2025-03-15 19:26:45 +0900

    feat: ProofHistory Status를 투표 결과에 따라 변경하는 스케줄러 작성

--- module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofHistoryRepository.java
+++ module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofHistoryRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ProofHistoryRepository extends JpaRepository<ProofHistory, Long> {
     Page<ProofHistory> findAllByProofId(Long proofId, Pageable pageable);
+    List<ProofHistory> findAllByProof_Date(LocalDate date);
 }
