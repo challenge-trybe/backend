36fe620cab93cec1fcec1c8390c4b5f1edd0fabd
rnignon <rnignon@naver.com>
2025-03-21 21:55:39 +0900

    feat: module-notification 모듈 생성

---
+++ module-notification/src/main/java/com/trybe/modulenotification/ModuleNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.modulenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleNotificationApplication.class, args);
+	}
+
+}
