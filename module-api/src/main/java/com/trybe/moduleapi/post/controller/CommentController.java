64b60d6771d9e870eb21ab851223dbefc8447f3a
minjiwon <mjo4260@naver.com>
2025-03-22 01:25:42 +0900

    feat: CommentController 추가

---
+++ module-api/src/main/java/com/trybe/moduleapi/post/controller/CommentController.java
@@ -0,0 +1,59 @@
+package com.trybe.moduleapi.post.controller;
+
+import com.trybe.moduleapi.auth.CustomUserDetails;
+import com.trybe.moduleapi.common.dto.PageResponse;
+import com.trybe.moduleapi.post.dto.CommentRequest;
+import com.trybe.moduleapi.post.dto.CommentResponse;
+import com.trybe.moduleapi.post.service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public CommentResponse.Detail enroll(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("postId") Long postId,
+            @Valid @RequestBody CommentRequest.Enroll request){
+        return commentService.enroll(userDetails.getUser(), postId, request);
+    }
+
+    @GetMapping("/comments/my")
+    public PageResponse<CommentResponse.Detail> findByMyComments(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            Pageable pageable){
+        return commentService.findByMyComments(userDetails.getUser(), pageable);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public PageResponse<CommentResponse.Summary> findAll(
+            @PathVariable("postId") Long postId,
+            Pageable pageable){
+        return commentService.findAll(postId, pageable);
+    }
+
+    @PutMapping("/comments/{commentId}")
+    public CommentResponse.Detail update(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("commentId") Long commentId,
+            @Valid @RequestBody CommentRequest.Update request){
+        return commentService.update(userDetails.getUser(), commentId, request);
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public void delete(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("commentId") Long commentId){
+        commentService.delete(userDetails.getUser(), commentId);
+
+    }
+}
