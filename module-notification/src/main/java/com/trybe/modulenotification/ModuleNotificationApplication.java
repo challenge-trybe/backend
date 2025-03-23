c48cead2e82db4d03917d01f36398d066ad5fa4d
rnignon <rnignon@naver.com>
2025-03-23 17:01:20 +0900

    feat: NotificationMessage, NotificationType 사용을 위한 어노테이션 수정

--- module-notification/src/main/java/com/trybe/modulenotification/ModuleNotificationApplication.java
+++ module-notification/src/main/java/com/trybe/modulenotification/ModuleNotificationApplication.java
@@ -3,7 +3,7 @@ package com.trybe.modulenotification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.trybe.modulenotification", "com.trybe.moduleapi.notification.dto", "com.trybe.modulecore.notification"})
 public class ModuleNotificationApplication {
 
 	public static void main(String[] args) {
