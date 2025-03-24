0b1cda0a50604264027b47b1100b11c1ddfe8c14
rnignon <rnignon@naver.com>
2025-03-24 20:44:18 +0900

    feat: ProofHistory 저장 시 Proof에 대한 해당 유저의 기록이 존재하는지 검증하는 로직 추가

--- module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofHistoryRepository.java
+++ module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.trybe.modulecore.proof.repository;
 
+import com.trybe.modulecore.proof.entity.Proof;
 import com.trybe.modulecore.proof.entity.ProofHistory;
+import com.trybe.modulecore.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +15,5 @@ import java.util.List;
 public interface ProofHistoryRepository extends JpaRepository<ProofHistory, Long> {
     Page<ProofHistory> findAllByProofId(Long proofId, Pageable pageable);
     List<ProofHistory> findAllByProof_Date(LocalDate date);
+    boolean existsByProofAndUser(Proof proof, User user);
 }
