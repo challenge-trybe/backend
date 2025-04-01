d3ef1031c11775b2de0d9dd1b6f9225e4a0053bf
rnignon <rnignon@naver.com>
2025-04-01 16:46:48 +0900

    feat: module-core에 대한 공통적인 application.yml 파일을 module-core의 application-core.yml에서 관리하도록 변경

--- module-recommendation/src/main/java/com/trybe/modulerecommendation/ModuleRecommendationApplication.java
+++ module-recommendation/src/main/java/com/trybe/modulerecommendation/ModuleRecommendationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModuleRecommendationApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application,application-core");
 		SpringApplication.run(ModuleRecommendationApplication.class, args);
 	}
 
