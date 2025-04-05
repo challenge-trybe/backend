6f758d839595f3e92ae0ea84e68497eb609973bb
minjiwon <mjo4260@naver.com>
2025-04-05 17:20:31 +0900

    feat: WebSocket 연결 엔드포인트 추가

--- module-api/src/main/java/com/trybe/moduleapi/config/SecurityConfig.java
+++ module-api/src/main/java/com/trybe/moduleapi/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/v1/posts/{id}").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/posts/search").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/posts/{postId}/comments").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/ws-connection")).permitAll() // 이걸 열어주는게 맞나~;;
                 .anyRequest()
                 .authenticated());
 
