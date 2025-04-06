14596acc32b8bb4beb1474cf68366f698b5867ec
minjiwon <mjo4260@naver.com>
2025-04-06 22:57:11 +0900

    fix: WebSocket 인증 실패 시 상태 코드를 401로 수정

--- module-api/src/main/java/com/trybe/moduleapi/auth/websocket/exception/WebSocketAccessDeniedException.java
+++ module-api/src/main/java/com/trybe/moduleapi/auth/websocket/exception/WebSocketAccessDeniedException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class WebSocketAccessDeniedException extends BusinessException {
     public WebSocketAccessDeniedException() {
-        super("로그인이 필요한 서비스입니다.", HttpStatus.FORBIDDEN.value());
+        super("로그인이 필요한 서비스입니다.", HttpStatus.UNAUTHORIZED.value());
     }
 }
