81503ef25bed170372ebc7be8742c9110d5ae3fd
minjiwon <mjo4260@naver.com>
2025-04-12 19:06:49 +0900

    feat: 게시글 좋아요 수정 - 게시글 좋아요 Redis Key 수정 - 게시글 좋아요 삭제 Redis key 추가 - 이에 따른 코드 수정

--- module-api/src/main/java/com/trybe/moduleapi/post/service/PostLikeService.java
+++ module-api/src/main/java/com/trybe/moduleapi/post/service/PostLikeService.java
@@ -10,18 +10,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class PostLikeService {
-    private final String USER_REDIS_PREFIX = "user";
-    private final String POST_REDIS_PREFIX = "post";
+    private final String POST_LIKE_SUFFIX = ":liked";
+    private final String USER_REDIS_KEY = "user:%d" + POST_LIKE_SUFFIX;
+    private final String POST_REDIS_KEY = "post:%d" + POST_LIKE_SUFFIX;
+    private final String POST_LIKE_DELETE_KEY = POST_REDIS_KEY + ":deleted";
     private final RedisTemplate<String, Long> redisTemplate;
     private final PostRepository postRepository;
 
@@ -34,14 +36,16 @@ public class PostLikeService {
     public PostResponse.Like addLike(User user, Long postId) {
         validateExistPost(postId);
 
-        String userKey = createRedisKey(USER_REDIS_PREFIX, user.getId());
-        String postKey = createRedisKey(POST_REDIS_PREFIX, postId);
+        String userKey = createRedisKey(USER_REDIS_KEY, user.getId());
+        String deleteKey = createRedisKey(POST_LIKE_DELETE_KEY, postId);
+        String postKey = createRedisKey(POST_REDIS_KEY, postId);
         int likeCount = count(postId);
 
         if (!alreadyLike(userKey, postId)) {
             double score = getCurrentTimeInSeconds();
             redisTemplate.opsForZSet().add(userKey, postId, score);
             redisTemplate.opsForSet().add(postKey, user.getId());
+            redisTemplate.opsForSet().remove(deleteKey, user.getId());
             likeCount++;
         }
         return PostResponse.Like.from(likeCount, true);
@@ -51,39 +55,45 @@ public class PostLikeService {
     public PostResponse.Like removeLike(User user, Long postId){
         validateExistPost(postId);
 
-        String userKey = createRedisKey(USER_REDIS_PREFIX, user.getId());
-        String postKey = createRedisKey(POST_REDIS_PREFIX, postId);
+        String userKey = createRedisKey(USER_REDIS_KEY, user.getId());
+        String deleteKey = createRedisKey(POST_LIKE_DELETE_KEY, postId);
+        String postKey = createRedisKey(POST_REDIS_KEY, postId);
+
         int likeCount = count(postId);
 
         if (alreadyLike(userKey, postId)) {
             redisTemplate.opsForZSet().remove(userKey, postId);
             redisTemplate.opsForSet().remove(postKey, user.getId());
+            redisTemplate.opsForSet().add(deleteKey, user.getId());
             likeCount--;
         }
         return PostResponse.Like.from(likeCount,false);
     }
 
     public void removeLikesByPost(Long postId){
-        String postKey = createRedisKey(POST_REDIS_PREFIX, postId);
+        String postKey = createRedisKey(POST_REDIS_KEY, postId);
         Set<Long> userIds = redisTemplate.opsForSet().members(postKey);
         if (userIds != null) {
             for (Long userId : userIds) {
-                String userKey = createRedisKey(USER_REDIS_PREFIX, userId);
+                String userKey = createRedisKey(USER_REDIS_KEY, userId);
                 redisTemplate.opsForZSet().remove(userKey, postId);
+
+                String deleteKey = createRedisKey(POST_LIKE_DELETE_KEY, postId);
+                redisTemplate.opsForSet().add(deleteKey, userId);
             }
             redisTemplate.delete(postKey);
         }
     }
 
     public int count(Long postId){
-        String postKey = createRedisKey(POST_REDIS_PREFIX, postId);
+        String postKey = createRedisKey(POST_REDIS_KEY, postId);
         Long count = redisTemplate.opsForSet().size(postKey);
         return count == null ? 0 : count.intValue();
     }
 
     @Transactional
     public PageResponse<PostResponse.Summary> getLikePostByUser(User user, Pageable pageable){
-        String userKey = createRedisKey(USER_REDIS_PREFIX, user.getId());
+        String userKey = createRedisKey(USER_REDIS_KEY, user.getId());
 
         Set<Long> postIds = redisTemplate.opsForZSet().reverseRange(userKey, 0, -1);
 
@@ -105,8 +115,8 @@ public class PostLikeService {
         return new PageResponse<>(likePostPages);
     }
 
-    private String createRedisKey(String domain, Long id){
-        return String.format("%s:%d", domain.toLowerCase(), id);
+    private String createRedisKey(String key, Long id){
+        return String.format(key, id);
     }
 
     private void validateExistPost(Long postId){
--- module-api/src/test/java/com/trybe/moduleapi/post/fixtures/PostLikeFixtures.java
+++ module-api/src/test/java/com/trybe/moduleapi/post/fixtures/PostLikeFixtures.java
@@ -11,4 +11,16 @@ public class PostLikeFixtures {
     public static final PostResponse.Like 좋아요_삭제_응답 = PostResponse.Like.from(4, false);
     public static final int 좋아요_개수 = 10;
     public static final Long 좋아요_개수L = 20L;
+
+    public static String createUserKey(Long userId) {
+        return String.format("user:%d:liked", userId);
+    }
+
+    public static String createDeletedKey(Long postId) {
+        return String.format("post:%d:liked:deleted", postId);
+    }
+
+    public static String createPostKey(Long postId) {
+        return String.format("post:%d:liked", postId);
+    }
 }
