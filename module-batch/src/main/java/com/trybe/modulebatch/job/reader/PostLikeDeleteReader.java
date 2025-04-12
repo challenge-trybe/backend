618ce7788b541767d7d5cbea3c6c5dfb914576ec
minjiwon <mjo4260@naver.com>
2025-04-12 19:21:08 +0900

    feat: 게시글 좋아요 삭제 기록 읽어오는 기능 구현

---
+++ module-batch/src/main/java/com/trybe/modulebatch/job/reader/PostLikeDeleteReader.java
@@ -0,0 +1,24 @@
+package com.trybe.modulebatch.job.reader;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostLikeDeleteReader extends PostLikeReader {
+    public PostLikeDeleteReader(RedisTemplate<String, Long> redisTemplate) {
+        super(redisTemplate);
+    }
+
+    private final String REDIS_SCAN_PATTERN = "post:*:liked:deleted";
+    private final String POST_LIKE_KEY = "post:%d:liked:deleted";
+
+    @Override
+    protected String getRedisScanPattern() {
+        return REDIS_SCAN_PATTERN;
+    }
+
+    @Override
+    protected String getPostRedisKey() {
+        return POST_LIKE_KEY;
+    }
+}
