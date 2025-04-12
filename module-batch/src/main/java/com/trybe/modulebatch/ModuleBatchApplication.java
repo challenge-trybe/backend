13dcd90e4b4a475eca4af21a8be75a93cbb2618b
minjiwon <mjo4260@naver.com>
2025-04-12 19:14:08 +0900

    feat: 배치 SpringApplication 초기화 및 설정

--- module-batch/src/main/java/com/trybe/modulebatch/ModuleBatchApplication.java
+++ module-batch/src/main/java/com/trybe/modulebatch/ModuleBatchApplication.java
@@ -1,13 +1,15 @@
 package com.trybe.modulebatch;
 
+import com.trybe.moduleapi.config.RedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.trybe.modulebatch", "com.trybe.modulecore"})
+@Import(RedisConfig.class)
 public class ModuleBatchApplication {
-
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(ModuleBatchApplication.class);
 		springApplication.setWebApplicationType(WebApplicationType.NONE);
@@ -15,5 +17,4 @@ public class ModuleBatchApplication {
 		int exitCode = SpringApplication.exit(context);
 		System.exit(exitCode);
 	}
-
 }
