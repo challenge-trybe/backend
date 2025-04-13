4ed1250156ce5457f3fa1942eb9cf140e5322b6d
rnignon <rnignon@naver.com>
2025-04-14 01:21:21 +0900

    feat: 유저 챌린지 조회 관련 캐시 구현

---
+++ module-core/src/main/java/com/trybe/modulecore/challenge/repository/view/ChallengeViewRedisCache.java
@@ -0,0 +1,27 @@
+package com.trybe.modulecore.challenge.repository.view;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+
+public class ChallengeViewRedisCache implements ChallengeViewCache {
+    private final RedisTemplate<String, Long> redisTemplate;
+
+    public ChallengeViewRedisCache(RedisTemplate<String, Long> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    private final String USER_VIEWED_CHALLENGE_KEY = "user:%d:viewed:challenge:%d";
+
+    @Override
+    public boolean hasViewed(Long userId, Long challengeId) {
+        String key = String.format(USER_VIEWED_CHALLENGE_KEY, userId, challengeId);
+        return redisTemplate.hasKey(key);
+    }
+
+    @Override
+    public void recordView(Long userId, Long challengeId) {
+        String key = String.format(USER_VIEWED_CHALLENGE_KEY, userId, challengeId);
+        redisTemplate.opsForValue().set(key, 1L, Duration.ofHours(3));
+    }
+}
