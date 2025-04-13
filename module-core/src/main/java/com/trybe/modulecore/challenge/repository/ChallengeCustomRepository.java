4cc0d6bf5f1a4b9a2b7a6bbcb622a57e1cfa1bf8
rnignon <rnignon@naver.com>
2025-04-13 19:52:19 +0900

    feat: 필터링 조건에 따라 챌린지 목록을 반환하는 메서드 추가

--- module-core/src/main/java/com/trybe/modulecore/challenge/repository/ChallengeCustomRepository.java
+++ module-core/src/main/java/com/trybe/modulecore/challenge/repository/ChallengeCustomRepository.java
@@ -2,9 +2,13 @@ package com.trybe.modulecore.challenge.repository;
 
 import com.trybe.modulecore.challenge.entity.Challenge;
 import com.trybe.modulecore.challenge.enums.ChallengeCategory;
+import com.trybe.modulecore.challenge.enums.ChallengeStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ChallengeCustomRepository {
+    Page<Challenge> getFilteredChallenges(String keyword, List<ChallengeCategory> categories, List<ChallengeStatus> statuses, Pageable pageable);
     List<Challenge> getByCategoriesOrKeywords(List<ChallengeCategory> categories, List<String> keywords, int limit);
 }
--- module-core/src/main/java/com/trybe/modulecore/challenge/repository/ChallengeCustomRepositoryImpl.java
+++ module-core/src/main/java/com/trybe/modulecore/challenge/repository/ChallengeCustomRepositoryImpl.java
@@ -1,15 +1,21 @@
 package com.trybe.modulecore.challenge.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.trybe.modulecore.challenge.entity.Challenge;
 import com.trybe.modulecore.challenge.enums.ChallengeCategory;
+import com.trybe.modulecore.challenge.enums.ChallengeStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 import static com.trybe.modulecore.challenge.entity.QChallenge.challenge;
+import static com.trybe.modulecore.utils.QueryUtils.getSort;
 import static com.trybe.modulecore.utils.QueryUtils.nullSafeBuilder;
 
 @Repository
@@ -18,6 +24,28 @@ public class ChallengeCustomRepositoryImpl implements ChallengeCustomRepository 
 
     public ChallengeCustomRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
         this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    @Override
+    public Page<Challenge> getFilteredChallenges(String keyword, List<ChallengeCategory> categories, List<ChallengeStatus> statuses, Pageable pageable) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        builder.and(titleOrDescriptionContains(keyword))
+                .and(categoryIn(categories))
+                .and(statusIn(statuses));
+
+        QueryResults<Challenge> challenges = jpaQueryFactory
+                .selectFrom(challenge)
+                .where(builder)
+                .orderBy(getSort(pageable, challenge))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<Challenge> content = challenges.getResults();
+        long total = challenges.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
     }
 
     @Override
@@ -34,13 +62,31 @@ public class ChallengeCustomRepositoryImpl implements ChallengeCustomRepository 
         return categoryIn(categories).or(titleContains(keywords));
     }
 
+    private BooleanBuilder titleContains(List<String> keywords) {
+        BooleanBuilder builder = new BooleanBuilder();
+        keywords.forEach(keyword -> builder.or(titleContains(keyword)));
+        return builder;
+    }
+
+    private BooleanBuilder titleOrDescriptionContains(String keyword) {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.or(titleContains(keyword)).or(descriptionContains(keyword));
+        return builder;
+    }
+
     private BooleanBuilder categoryIn(List<ChallengeCategory> categories) {
         return nullSafeBuilder(() -> challenge.category.in(categories));
     }
 
-    private BooleanBuilder titleContains(List<String> keywords) {
-        BooleanBuilder builder = new BooleanBuilder();
-        keywords.forEach(keyword -> builder.or(challenge.title.containsIgnoreCase(keyword)));
-        return builder;
+    private BooleanBuilder statusIn(List<ChallengeStatus> statuses) {
+        return nullSafeBuilder(() -> challenge.status.in(statuses));
+    }
+
+    private BooleanBuilder titleContains(String keyword) {
+        return nullSafeBuilder(() -> challenge.title.containsIgnoreCase(keyword));
+    }
+
+    private BooleanBuilder descriptionContains(String keyword) {
+        return nullSafeBuilder(() -> challenge.description.containsIgnoreCase(keyword));
     }
 }
