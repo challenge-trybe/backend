ee3760dda7dabd30af064b8a05071785d3b88277
rnignon <rnignon@naver.com>
2025-04-16 15:57:30 +0900

    chore: RedisConfig의 module-api -> module-core로의 이동에 따른 ModuleBatchApplication.java 코드 수정

--- module-batch/src/main/java/com/trybe/modulebatch/ModuleBatchApplication.java
+++ module-batch/src/main/java/com/trybe/modulebatch/ModuleBatchApplication.java
@@ -1,20 +1,19 @@
 package com.trybe.modulebatch;
 
-import com.trybe.moduleapi.config.RedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"com.trybe.modulebatch", "com.trybe.modulecore"})
-@Import(RedisConfig.class)
 public class ModuleBatchApplication {
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(ModuleBatchApplication.class);
 		springApplication.setWebApplicationType(WebApplicationType.NONE);
+
 		ConfigurableApplicationContext context = springApplication.run(args);
 		int exitCode = SpringApplication.exit(context);
+
 		System.exit(exitCode);
 	}
 }
