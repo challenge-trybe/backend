9d073b3f37a0e02f84c38e16e88cec616ad29742
minjiwon <mjo4260@naver.com>
2025-04-18 06:48:28 +0900

    feat: 인기글 집계 비동기 처리를 위한 Cofig 작성

---
+++ module-api/src/main/java/com/trybe/moduleapi/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.trybe.moduleapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
