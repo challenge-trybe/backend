165bd96157ae3131a9c7d34268ebf58b1187e56f
minjiwon <mjo4260@naver.com>
2025-04-19 19:28:58 +0900

    test: Redis 접근 로직 분리에 따른 테스트 코드 수정

--- module-api/src/test/java/com/trybe/moduleapi/post/service/PostLikeServiceTest.java
+++ module-api/src/test/java/com/trybe/moduleapi/post/service/PostLikeServiceTest.java
@@ -7,6 +7,7 @@ import com.trybe.moduleapi.post.fixtures.PostFixtures;
 import com.trybe.moduleapi.post.fixtures.PostLikeFixtures;
 import com.trybe.moduleapi.user.fixtures.UserFixtures;
 import com.trybe.modulecore.post.entity.Post;
+import com.trybe.modulecore.post.repository.PostLikeCache;
 import com.trybe.modulecore.post.repository.PostRepository;
 import com.trybe.modulecore.user.entity.User;
 import org.junit.jupiter.api.DisplayName;
@@ -17,9 +18,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.ZSetOperations;
 
 import java.util.List;
 import java.util.Set;
@@ -31,7 +29,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class PostLikeServiceTest {
     @Mock
-    private RedisTemplate<String, Long> redisTemplate;
+    private PostLikeCache postLikeCache;
     @Mock
     private PostRepository postRepository;
 
@@ -42,28 +40,19 @@ class PostLikeServiceTest {
     @DisplayName("게시글에 좋아요를 누르면 Redis에 저장된다")
     void 게시글에_좋아요를_누르면_Redis에_저장된다() {
         // given
-        mockingRedisTemplate();
-
         User 회원 = UserFixtures.회원;
-        String userKey = PostLikeFixtures.createUserKey(회원.getId());
-
         Long 포스트_ID = PostFixtures.id;
-        String postKey = PostLikeFixtures.createPostKey(포스트_ID);
-        String deleteKey = PostLikeFixtures.createDeletedKey(포스트_ID);
 
-        Long 좋아요_개수 = PostLikeFixtures.좋아요_개수L;
-
+        int 좋아요_개수 = PostLikeFixtures.좋아요_개수;
         when(postRepository.existsById(포스트_ID)).thenReturn(true);
-        when(redisTemplate.opsForZSet().score(userKey, 포스트_ID)).thenReturn(null);
-        when(redisTemplate.opsForSet().size(postKey)).thenReturn(좋아요_개수);
+        when(postLikeCache.addLike(회원.getId(), 포스트_ID)).thenReturn(좋아요_개수);
 
         // when
         PostResponse.Like 응답 = postLikeService.addLike(회원, PostFixtures.id);
 
         // then
-        assertEquals(응답.likeCount(), 좋아요_개수+1);
+        assertEquals(응답.likeCount(), 좋아요_개수);
         assertEquals(응답.isLiked(), true);
-        verify(redisTemplate.opsForSet()).remove(eq(deleteKey), eq(회원.getId()));
     }
 
     @Test
@@ -82,30 +71,18 @@ class PostLikeServiceTest {
     @DisplayName("게시글에 대한 좋아요를 삭제하면 Redis에서 삭제된다.")
     void 게시글에_대한_좋아요를_삭제하면_Redis에서_삭제된다() {
         // given
-        mockingRedisTemplate();
-
         User 회원 = UserFixtures.회원;
-        String userKey = PostLikeFixtures.createUserKey(회원.getId());
-
         Long 포스트_ID = PostFixtures.id;
-        String postKey = PostLikeFixtures.createPostKey(포스트_ID);
-        String deleteKey = PostLikeFixtures.createDeletedKey(포스트_ID);
 
-        Long 좋아요_개수 = PostLikeFixtures.좋아요_개수L;
-
+        int 좋아요_개수 = PostLikeFixtures.좋아요_개수;
         when(postRepository.existsById(포스트_ID)).thenReturn(true);
-        when(redisTemplate.opsForZSet().score(userKey, 포스트_ID)).thenReturn(1.000);
-        when(redisTemplate.opsForSet().size(postKey)).thenReturn(좋아요_개수);
-
+        when(postLikeCache.removeLike(회원.getId(), 포스트_ID)).thenReturn(좋아요_개수);
 
         // when
         PostResponse.Like 응답 = postLikeService.removeLike(회원, PostFixtures.id);
 
         // then
-        verify(redisTemplate.opsForZSet()).remove(eq(userKey), eq(PostFixtures.id));
-        verify(redisTemplate.opsForSet()).remove(eq(postKey), eq(회원.getId()));
-        verify(redisTemplate.opsForSet()).add(eq(deleteKey), eq(회원.getId()));
-        assertEquals(응답.likeCount(), 좋아요_개수-1);
+        assertEquals(응답.likeCount(), 좋아요_개수);
         assertEquals(응답.isLiked(), false);
     }
 
@@ -126,60 +103,43 @@ class PostLikeServiceTest {
     @DisplayName("게시글이 삭제되면 해당 게시글에 대한 모든 좋아요는 삭제된다.")
     void 게시글이_삭제되면_해당_게시글에_대한_모든_좋아요는_삭제된다() {
         // given
-        mockingRedisTemplate();
-
         Long 포스트_ID = PostFixtures.id;
-        String postKey = PostLikeFixtures.createPostKey(포스트_ID);
-        Set<Long> userIds = PostLikeFixtures.게시글_좋아요_누른_유저목록;
-        when(redisTemplate.opsForSet().members(postKey)).thenReturn(userIds);
 
         // When
-        postLikeService.removeLikesByPost(PostFixtures.id);
+        postLikeService.removeLikesByPost(포스트_ID);
 
         // Then
-        for (Long userId : userIds) {
-            String userKey = PostLikeFixtures.createUserKey(userId);
-            verify(redisTemplate.opsForZSet(), times(1)).remove(eq(userKey), eq(PostFixtures.id));
-
-            String deleteKey = PostLikeFixtures.createDeletedKey(포스트_ID);
-            verify(redisTemplate.opsForSet()).add(eq(deleteKey), eq(userId));
-        }
-        verify(redisTemplate, times(1)).delete(postKey);
+        verify(postLikeCache, times(1)).removeLikesByPost(포스트_ID);
     }
 
     @Test
     @DisplayName("특정 게시글의 좋아요 수를 조회하면 좋아요 개수가 반환된다.")
     void 특정_게시글의_좋아요_수를_조회하면_좋아요_개수가_반환된다() {
         // given
-        SetOperations<String, Long> setOps = mock(SetOperations.class);
-        when(redisTemplate.opsForSet()).thenReturn(setOps);
-
         Long 포스트_ID = PostFixtures.id;
-        String postKey = PostLikeFixtures.createPostKey(포스트_ID);
-        when(redisTemplate.opsForSet().size(postKey)).thenReturn(5L);
+        int 좋아요_개수 = PostLikeFixtures.좋아요_개수;
+        when(postLikeCache.getPostLikeCount(포스트_ID)).thenReturn(좋아요_개수);
 
         // when
-        int likeCount = postLikeService.count(PostFixtures.id);
+        int likeCount = postLikeService.getPostLikeCount(PostFixtures.id);
 
         // then
-        assertEquals(likeCount, 5);
+        assertEquals(likeCount, 좋아요_개수);
     }
 
     @Test
     @DisplayName("회원이 자신이 좋아요 누른 게시글을 조회하면 좋아요 누른 순으로 반환된다.")
     void 회원이_자신이_좋아요_누른_게시글을_조회하면_좋아요_누른_순으로_반환된다() {
         // given
-        ZSetOperations<String, Long> zSetOps = mock(ZSetOperations.class);
-        when(redisTemplate.opsForZSet()).thenReturn(zSetOps);
-
         Pageable pageable = PageRequest.of(0, 10);
+        int start = pageable.getPageNumber() * pageable.getPageSize();
+        int end = start + pageable.getPageSize() - 1;
 
         User 회원 = UserFixtures.회원;
-        String userKey = PostLikeFixtures.createUserKey(회원.getId());
         Set<Long> postIds = PostFixtures.게시글_아이디_목록;
         List<Post> 게시글_목록 = PostFixtures.ID_존재하는_게시글_목록;
 
-        when(redisTemplate.opsForZSet().reverseRange(userKey, 0, -1)).thenReturn(postIds);
+        when(postLikeCache.getLikePostIdsByUser(회원.getId(), start, end)).thenReturn(postIds);
         when(postRepository.findAllByIdIn(postIds)).thenReturn(게시글_목록);
 
         // When
@@ -187,13 +147,5 @@ class PostLikeServiceTest {
 
         // Then
         assertEquals(응답.content().size(), 게시글_목록.size());
-    }
-
-    private void mockingRedisTemplate(){
-        ZSetOperations<String, Long> zSetOps = mock(ZSetOperations.class);
-        SetOperations<String, Long> setOps = mock(SetOperations.class);
-
-        when(redisTemplate.opsForZSet()).thenReturn(zSetOps);
-        when(redisTemplate.opsForSet()).thenReturn(setOps);
     }
 }
