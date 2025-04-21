318b662b267d7af8f0224353aeb1cabfd25e64e4
rnignon <rnignon@naver.com>
2025-04-21 14:53:00 +0900

    feat: FileResponse에 savedName 필드 제거

--- module-api/src/main/java/com/trybe/moduleapi/file/dto/FileResponse.java
+++ module-api/src/main/java/com/trybe/moduleapi/file/dto/FileResponse.java
@@ -2,10 +2,9 @@ package com.trybe.moduleapi.file.dto;
 
 public record FileResponse(
         String originalName,
-        String savedName,
         String filePath
 ) {
-    public static FileResponse from(String originalName, String savedName, String filePath){
-        return new FileResponse(originalName, savedName, filePath);
+    public static FileResponse from(String originalName, String filePath){
+        return new FileResponse(originalName, filePath);
     }
 }
