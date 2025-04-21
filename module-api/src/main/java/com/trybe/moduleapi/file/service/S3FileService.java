087f4d14e41efc6cf145841144dd36a9bfb7480c
rnignon <rnignon@naver.com>
2025-04-21 16:37:03 +0900

    feat: FileService 메서드 간소화에 따른 S3FileService 코드 수정

--- module-api/src/main/java/com/trybe/moduleapi/file/service/S3FileService.java
+++ module-api/src/main/java/com/trybe/moduleapi/file/service/S3FileService.java
@@ -2,7 +2,7 @@ package com.trybe.moduleapi.file.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.trybe.moduleapi.file.dto.FileResponse;
+import com.trybe.moduleapi.file.dto.FileMeta;
 import com.trybe.moduleapi.file.exception.FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -11,8 +11,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -30,37 +28,24 @@ public class S3FileService implements FileService {
     }
 
     @Override
-    public List<FileResponse> uploadFiles(List<MultipartFile> files, String basePath) {
-        List<FileResponse> fileResponses = new ArrayList<>();
-        for (MultipartFile file : files) {
-            String fileName = generateFileName(file);
-            String filePath = basePath + "/" + fileName;
+    public FileMeta uploadFile(MultipartFile file, String basePath) {
+        String fileName = generateFileName(file);
+        String filePath = basePath + "/" + fileName;
 
-            try {
-                ObjectMetadata metadata = new ObjectMetadata();
-                metadata.setContentLength(file.getSize());
-                metadata.setContentType(file.getContentType());
-                amazonS3Client.putObject(bucket, filePath, file.getInputStream(), metadata);
-                FileResponse fileResponse = FileResponse.from(file.getOriginalFilename(), fileName, filePath);
-                fileResponses.add(fileResponse);
-            } catch (IOException e) {
-                throw new FileUploadException();
-            }
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+            amazonS3Client.putObject(bucket, filePath, file.getInputStream(), metadata);
+            return new FileMeta(file.getOriginalFilename(), fileName, filePath);
+        } catch (IOException e) {
+            throw new FileUploadException();
         }
-        return fileResponses;
     }
 
     @Override
     public String getFileUrl(String filePath) {
         return String.format(S3baseUrl, bucket, amazonS3Client.getRegionName(), filePath);
-    }
-
-    @Override
-    public List<FileResponse> updateFiles(List<String> filePaths, List<MultipartFile> newFiles, String basePath) {
-        for (String filePath : filePaths) {
-            deleteFile(filePath);
-        }
-        return uploadFiles(newFiles, basePath);
     }
 
     @Override
