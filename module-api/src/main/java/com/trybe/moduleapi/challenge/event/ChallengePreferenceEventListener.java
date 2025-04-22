d30e66d6963394055e909ae54218357407947503
rnignon <rnignon@naver.com>
2025-04-23 02:57:25 +0900

    refactor: 챌린지 인기 점수를 조정하는 이벤트 리스너와 유저의 챌린지 선호도 점수를 조정하는 이벤트 리스너 분리

---
+++ module-api/src/main/java/com/trybe/moduleapi/challenge/event/ChallengePreferenceEventListener.java
@@ -0,0 +1,35 @@
+package com.trybe.moduleapi.challenge.event;
+
+import com.trybe.modulecore.challenge.entity.Challenge;
+import com.trybe.modulecore.challenge.repository.preference.ChallengePreferenceCache;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+public class ChallengePreferenceEventListener {
+    private final ChallengePreferenceCache challengePreferenceCache;
+
+    public ChallengePreferenceEventListener(ChallengePreferenceCache challengePreferenceCache) {
+        this.challengePreferenceCache = challengePreferenceCache;
+    }
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleChallengeEvent(ChallengeEvent event) {
+        Challenge challenge = event.getChallenge();
+        Long userId = event.getUserId();
+
+        ChallengeEventType type = event.getType();
+        int score = event.getType().getActionType().getPreferenceScore();
+
+        if (score == 0) { return; }
+
+        if (type.isScoreUp()) {
+            challengePreferenceCache.addPreference(userId, challenge, score);
+        } else {
+            challengePreferenceCache.removePreference(userId, challenge, score);
+        }
+    }
+}
--- module-api/src/main/java/com/trybe/moduleapi/challenge/event/PopularChallengeEventListener.java
+++ module-api/src/main/java/com/trybe/moduleapi/challenge/event/PopularChallengeEventListener.java
@@ -1,46 +1,34 @@
 package com.trybe.moduleapi.challenge.event;
 
 import com.trybe.moduleapi.utils.DateUtils;
-import com.trybe.modulecore.challenge.entity.Challenge;
 import com.trybe.modulecore.challenge.repository.popular.PopularChallengeCache;
-import com.trybe.modulecore.challenge.repository.preference.ChallengePreferenceCache;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
-public class ChallengeEventListener {
+public class PopularChallengeEventListener {
     private final PopularChallengeCache popularChallengeCache;
-    private final ChallengePreferenceCache challengePreferenceCache;
 
-    public ChallengeEventListener(PopularChallengeCache popularChallengeCache, ChallengePreferenceCache challengePreferenceCache) {
+    public PopularChallengeEventListener(PopularChallengeCache popularChallengeCache) {
         this.popularChallengeCache = popularChallengeCache;
-        this.challengePreferenceCache = challengePreferenceCache;
     }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleChallengeEvent(ChallengeEvent event) {
-        Challenge challenge = event.getChallenge();
-
-        Long challengeId = challenge.getId();
-        Long userId = event.getUserId();
+        Long challengeId = event.getChallenge().getId();
 
         ChallengeEventType type = event.getType();
-        int score = type.getActionType().getScore();
+        int score = type.getActionType().getPopularityScore();
 
-        if (type.getActionType() == ChallengeActionType.CREATE) {
-            increasePreference(userId, challenge, score);
-            return;
-        }
+        if (score == 0) { return; }
 
         if (type.isScoreUp()) {
             increasePopularity(challengeId, score);
-            increasePreference(userId, challenge, score);
         } else {
             decreasePopularity(challengeId, score);
-            decreasePreference(userId, challenge, score);
         }
     }
 
@@ -50,13 +38,5 @@ public class ChallengeEventListener {
 
     private void decreasePopularity(Long challengeId, int score) {
         popularChallengeCache.decreaseScore(challengeId, score, DateUtils.getToday());
-    }
-
-    private void increasePreference(Long userId, Challenge challenge, int score) {
-        challengePreferenceCache.addPreference(userId, challenge, score);
-    }
-
-    private void decreasePreference(Long userId, Challenge challenge, int score) {
-        challengePreferenceCache.removePreference(userId, challenge, score);
     }
 }
