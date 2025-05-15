5028fe4f7e89de72ee7536f63281dd1ecd9b1427
rnignon <rnignon@naver.com>
2025-05-15 22:34:40 +0900

    feat: ProofHistory 첨부파일 순서를 나타내는 fileOrder 필드 추가

--- module-api/src/main/java/com/trybe/moduleapi/proof/dto/request/ProofHistoryRequest.java
+++ module-api/src/main/java/com/trybe/moduleapi/proof/dto/request/ProofHistoryRequest.java
@@ -6,6 +6,8 @@ import com.trybe.modulecore.user.entity.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.List;
+
 public class ProofHistoryRequest {
     private static final String CONTENT_NOT_BLANK_MESSAGE = "인증 내용을 입력해주세요.";
     private static final String CONTENT_MAX_LENGTH_MESSAGE = "인증 내용은 최대 1,000자까지 입력 가능합니다.";
@@ -13,7 +15,8 @@ public class ProofHistoryRequest {
     public record Create(
             @NotBlank(message = CONTENT_NOT_BLANK_MESSAGE)
             @Size(max = 1000, message = CONTENT_MAX_LENGTH_MESSAGE)
-            String content
+            String content,
+            List<Integer> fileOrder
     ) {
         public ProofHistory toEntity(Proof proof, User user, String content) {
             return new ProofHistory(proof, user, content);
@@ -23,6 +26,7 @@ public class ProofHistoryRequest {
     public record Update(
             @NotBlank(message = CONTENT_NOT_BLANK_MESSAGE)
             @Size(max = 1000, message = CONTENT_MAX_LENGTH_MESSAGE)
-            String content
+            String content,
+            List<Integer> fileOrder
     ) {}
 }
