888d5458ee8450f0e41133172178c9b266e959f9
rnignon <rnignon@naver.com>
2025-05-23 22:12:04 +0900

    test: FileFixtures 내 FileWithIdResponse 값 수정 및 연관 파일 ID 상수 추가

--- module-api/src/test/java/com/trybe/moduleapi/file/fixtures/FileFixtures.java
+++ module-api/src/test/java/com/trybe/moduleapi/file/fixtures/FileFixtures.java
@@ -2,7 +2,6 @@ package com.trybe.moduleapi.file.fixtures;
 
 import com.trybe.moduleapi.file.dto.FileResponse;
 import com.trybe.moduleapi.file.dto.FileWithIdResponse;
-import com.trybe.moduleapi.proof.fixtures.ProofHistoryFixtures;
 import com.trybe.modulecore.file.entity.File;
 import org.springframework.mock.web.MockMultipartFile;
 
@@ -11,6 +10,7 @@ import java.util.UUID;
 
 public class FileFixtures {
     public static final Long 파일_ID = 1L;
+    public static final Long 연관_파일_ID = 1L;
     public static final String 파일_원본_이름 = "originalName.jpeg";
     public static final String 파일_저장_이름 = UUID.randomUUID().toString() + "-" + 파일_원본_이름;
     public static final String 파일_경로 = "/filePath/" + 파일_저장_이름;
@@ -43,6 +43,6 @@ public class FileFixtures {
 
     /* Response DTO */
     public static final FileResponse 파일_응답 = new FileResponse(파일_원본_이름, 파일_전체_경로);
-    public static final FileWithIdResponse 아이디_포함_파일_응답 = FileWithIdResponse.from(ProofHistoryFixtures.인증_기록_ID, 파일, 파일_전체_경로);
+    public static final FileWithIdResponse 아이디_포함_파일_응답 = new FileWithIdResponse(연관_파일_ID, 파일_원본_이름, 파일_전체_경로);
     public static final List<FileWithIdResponse> 아이디_포함_파일_목록_응답 = List.of(아이디_포함_파일_응답);
 }
