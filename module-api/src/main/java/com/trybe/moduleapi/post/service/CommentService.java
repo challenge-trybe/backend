d15fa19ca78aef573824a42066aced54259c6557
minjiwon <mjo4260@naver.com>
2025-05-27 21:45:51 +0900

    refactor: 알림 관련 메서드명 및 상수명 통일

--- module-api/src/main/java/com/trybe/moduleapi/post/service/CommentService.java
+++ module-api/src/main/java/com/trybe/moduleapi/post/service/CommentService.java
@@ -30,7 +30,7 @@ public class CommentService {
     private final NotificationProducerService notificationProducerService;
 
     private static final String COMMENT_TITLE = "새로운 댓글이 달렸습니다.";
-    private static final String COMMENT_NOTICE_MESSAGE_FORMAT = "[%s] 님이 게시글에 댓글을 달았습니다.";
+    private static final String COMMENT_NOTIFICATION_MESSAGE_FORMAT = "[%s] 님이 게시글에 댓글을 달았습니다.";
     public CommentService(CommentRepository commentRepository, PostRepository postRepository, PostEventPublisher eventPublisher, NotificationProducerService notificationProducerService) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
@@ -44,7 +44,7 @@ public class CommentService {
         Comment comment = request.toEntity(user, post, request.content());
         commentRepository.save(comment);
 
-        String message = createNotifyMessage(user.getNickname());
+        String message = createNotificationMessage(user.getNickname());
         Notification notification = new Notification(post.getUser().getId(), NotificationType.POST_COMMENT, postId, COMMENT_TITLE, message);
         notificationProducerService.publishPostCommentNotification(post.getUser().getUuid(), notification);
 
@@ -95,8 +95,8 @@ public class CommentService {
         return postRepository.findById(id).orElseThrow(() -> new NotFoundPostException());
     }
 
-    private String createNotifyMessage(String nickName) {
-        return String.format(COMMENT_NOTICE_MESSAGE_FORMAT, nickName);
+    private String createNotificationMessage(String nickName) {
+        return String.format(COMMENT_NOTIFICATION_MESSAGE_FORMAT, nickName);
     }
 
 }
