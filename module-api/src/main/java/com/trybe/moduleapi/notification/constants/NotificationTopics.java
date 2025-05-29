d3eee2a5cd040c12d7c9b619104b02ad992f78e7
rnignon <rnignon@naver.com>
2025-05-29 20:29:40 +0900

    feat: Notification Topic 공통 상수 클래스 추가

---
+++ module-api/src/main/java/com/trybe/moduleapi/notification/constants/NotificationTopics.java
@@ -0,0 +1,6 @@
+package com.trybe.moduleapi.notification.constants;
+
+public class NotificationTopics {
+    public static final String CHALLENGE = "Challenge_Notification";
+    public static final String CHALLENGE_PROOF = "Challenge_Proof_Notification";
+}
