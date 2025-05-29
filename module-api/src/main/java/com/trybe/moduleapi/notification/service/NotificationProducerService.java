54dd013554b307fc059fb86350b1ee56c1cf2d93
rnignon <rnignon@naver.com>
2025-05-29 20:31:34 +0900

    feat: publishNotification, publishNotifications 메서드 추가

--- module-api/src/main/java/com/trybe/moduleapi/notification/service/NotificationProducerService.java
+++ module-api/src/main/java/com/trybe/moduleapi/notification/service/NotificationProducerService.java
@@ -1,8 +1,15 @@
 package com.trybe.moduleapi.notification.service;
 
 import com.trybe.moduleapi.notification.dto.NotificationMessage;
+import com.trybe.modulecore.notification.entity.Notification;
+import com.trybe.modulecore.notification.enums.NotificationType;
+import com.trybe.modulecore.user.entity.User;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NotificationProducerService {
@@ -12,5 +19,43 @@ public class NotificationProducerService {
     public NotificationProducerService(KafkaTemplate<String, NotificationMessage> kafkaTemplate, NotificationService notificationService) {
         this.kafkaTemplate = kafkaTemplate;
         this.notificationService = notificationService;
+    }
+
+    @Transactional
+    public void publishNotification(
+            String topic,
+            User receiver,
+            NotificationType type,
+            Long typeId,
+            String title,
+            String message
+    ) {
+        Notification notification = new Notification(receiver.getId(), type, typeId, title, message);
+        NotificationMessage notificationMessage = NotificationMessage.from(notification, receiver.getUuid());
+        
+        kafkaTemplate.send(topic, notificationMessage);
+        notificationService.save(notification);
+    }
+
+    @Transactional
+    public void publishNotifications(
+            String topic,
+            List<User> receivers,
+            NotificationType type,
+            Long typeId,
+            String title,
+            String message
+    ) {
+        List<Notification> notifications = new ArrayList<>();
+
+        for (User receiver : receivers) {
+            Notification notification = new Notification(receiver.getId(), type, typeId, title, message);
+            notifications.add(notification);
+
+            NotificationMessage notificationMessage = NotificationMessage.from(notification, receiver.getUuid());
+            kafkaTemplate.send(topic, notificationMessage);
+        }
+
+        notificationService.saveAll(notifications);
     }
 }
