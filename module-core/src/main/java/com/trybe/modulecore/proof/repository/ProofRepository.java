149f187915205d4b018fcd015d295a2773a59494
rnignon <rnignon@naver.com>
2025-06-11 03:38:33 +0900

    feat: Proof 스케줄러 내 시작 및 종료 알림을 위한 findAllByDate 메서드 추가

--- module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofRepository.java
+++ module-core/src/main/java/com/trybe/modulecore/proof/repository/ProofRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ProofRepository extends JpaRepository<Proof, Long> {
     boolean existsByChallengeIdAndDate(Long challengeId, LocalDate date);
     int countByChallengeId(Long challengeId);
     Page<Proof> findAllByChallengeId(Long challengeId, Pageable pageable);
+    List<Proof> findAllByDate(LocalDate date);
 }
